57105349ae37bceaaec5a8ac5d625c10f3b0a452
Fornitx <49841725+Fornitx@users.noreply.github.com>
2023-05-23 20:23:18 +0300

    tls example

--- src/main/kotlin/com/example/ClientMain.kt
+++ src/main/kotlin/com/example/ClientMain.kt
@@ -2,12 +2,45 @@ package com.example
 
 import foo.bar.GreeterGrpc
 import foo.bar.GreeterOuterClass.HelloRequest
+import io.grpc.ChannelCredentials
 import io.grpc.Grpc
 import io.grpc.InsecureChannelCredentials
+import io.grpc.TlsChannelCredentials
+import java.io.File
+import java.security.KeyStore
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.TrustManagerFactory
+
+private val password = "password".toCharArray()
 
 fun main() {
-    val channel = Grpc.newChannelBuilder("localhost:8080", InsecureChannelCredentials.create()).build()
+    val channel = Grpc.newChannelBuilder("localhost:8080", credentials(false)).build()
     val stub = GreeterGrpc.newBlockingStub(channel)
     val reply = stub.sayHello(HelloRequest.newBuilder().setName("Abc").build())
     println("ClientMain.sayHello $reply")
+}
+
+fun credentials(secure: Boolean): ChannelCredentials? = if (secure) {
+    TlsChannelCredentials.newBuilder()
+        .keyManager(*keyManagerFactory().keyManagers)
+        .trustManager(*trustManagerFactory().trustManagers)
+        .build()
+} else {
+    InsecureChannelCredentials.create()
+}
+
+fun keyManagerFactory(): KeyManagerFactory {
+    val ks = KeyStore.getInstance("PKCS12")
+    File("keystore.p12").inputStream().use {
+        ks.load(it, password)
+    }
+    return KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm()).apply { init(ks, password) }
+}
+
+fun trustManagerFactory(): TrustManagerFactory {
+    val ts = KeyStore.getInstance("PKCS12")
+    File("truststore.p12").inputStream().use {
+        ts.load(it, password)
+    }
+    return TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm()).apply { init(ts) }
 }
--- src/main/kotlin/com/example/ClientMainKt.kt
+++ src/main/kotlin/com/example/ClientMainKt.kt
@@ -3,11 +3,10 @@ package com.example
 import foo.bar.GreeterGrpcKt
 import foo.bar.helloRequest
 import io.grpc.Grpc
-import io.grpc.InsecureChannelCredentials
 import kotlinx.coroutines.runBlocking
 
 fun main() {
-    val channel = Grpc.newChannelBuilder("localhost:8080", InsecureChannelCredentials.create()).build()
+    val channel = Grpc.newChannelBuilder("localhost:8080", credentials(false)).build()
     val stub = GreeterGrpcKt.GreeterCoroutineStub(channel)
     runBlocking {
         val reply = stub.sayHello(helloRequest { name = "Abc" })
