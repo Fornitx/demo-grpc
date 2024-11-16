a048684a351905c30a11625fff1b0ec53ec6c149
Fornitx <49841725+Fornitx@users.noreply.github.com>
2024-11-16 21:47:00 +0300

    fix System properties (again 2)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,11 +13,11 @@ dependencyResolutionManagement {
         create("libs") {
             from(files("gradle/custom.libs.versions.toml"))
 
-            version("kotlin", System.getProperty("kotlin.version"))
-            version("kotlin-coroutines", System.getProperty("kotlin.coroutines.version"))
-            version("kotlin-logging", System.getProperty("kotlin.logging.version"))
-            version("spring-boot", System.getProperty("spring.boot.version"))
-            version("spring-dm", System.getProperty("spring.dm.version"))
+            version("kotlin", providers.gradleProperty("kotlin.version").get())
+            version("kotlin-coroutines", providers.gradleProperty("kotlin-coroutines.version").get())
+            version("kotlin-logging", providers.gradleProperty("kotlin-logging.version").get())
+            version("spring-boot", providers.gradleProperty("spring-boot.version").get())
+            version("spring-dm", providers.gradleProperty("spring-dm.version").get())
         }
     }
 }
