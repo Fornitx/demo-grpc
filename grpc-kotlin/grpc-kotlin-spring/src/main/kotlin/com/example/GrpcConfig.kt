494bd0592f7c3a9e3333546f1254f74d01b41b03
Fornitx <49841725+Fornitx@users.noreply.github.com>
2025-05-21 21:00:21 +0300

    GrpcService => Bean

--- grpc-kotlin/grpc-kotlin-spring/src/main/kotlin/com/example/GrpcConfig.kt
+++ grpc-kotlin/grpc-kotlin-spring/src/main/kotlin/com/example/GrpcConfig.kt
@@ -1,19 +1,15 @@
 package com.example
 
-import com.example.foo.bar.Greeter1GrpcKt
-import com.example.foo.bar.Greeter2GrpcKt
-import com.example.foo.bar.HelloReply
-import com.example.foo.bar.HelloRequest
-import com.example.foo.bar.helloReply
+import com.example.foo.bar.*
 import com.example.utils.LoggingUtils.log
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.grpc.server.service.GrpcService
 
 @Configuration
 class GrpcConfig {
-    @GrpcService
+    @Bean
     fun greeter1Service(): Greeter1GrpcKt.Greeter1CoroutineImplBase =
         object : Greeter1GrpcKt.Greeter1CoroutineImplBase() {
             override suspend fun sayHello(request: HelloRequest): HelloReply {
@@ -22,7 +18,7 @@ class GrpcConfig {
             }
         }
 
-    @GrpcService
+    @Bean
     fun greeter2Service(): Greeter2GrpcKt.Greeter2CoroutineImplBase =
         object : Greeter2GrpcKt.Greeter2CoroutineImplBase() {
             override fun sayHello(requests: Flow<HelloRequest>): Flow<HelloReply> = flow {
